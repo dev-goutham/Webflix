1f1345512f98009a947775ef7d8a44eff9558cbd
dev-goutham <dev.gouthamram95@gmail.com>
2022-10-18 11:05:30 +0530

    added toast component

--- src/components/BookmarkComponent/index.tsx
+++ src/components/BookmarkComponent/index.tsx
@@ -1,6 +1,8 @@
+import { useAuth } from '@/hooks/useAuth';
 import useBookmarks from '@/hooks/useBookmarks';
 import React, { PropsWithChildren, useEffect, useState } from 'react';
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import { Toaster, toast } from 'react-hot-toast';
 
 interface Props extends PropsWithChildren {
   id: number;
@@ -8,6 +10,7 @@ interface Props extends PropsWithChildren {
 }
 
 const BookmarkComponent: React.FC<Props> = ({ id, type }) => {
+  const { isAuthenticated } = useAuth();
   const { bookmarks, addBookmarks, removeBookmark } = useBookmarks();
   const [isBookmarked, setIsBookmarked] = useState<boolean | null>(null);
   useEffect(() => {
@@ -20,27 +23,36 @@ const BookmarkComponent: React.FC<Props> = ({ id, type }) => {
   }, [bookmarks.movies.length, bookmarks.tv.length]);
 
   const clickHandler = () => {
+    if (!isAuthenticated) {
+      toast.error('Login to bookmark your favourite movies and tv shows');
+      return;
+    }
     if (isBookmarked !== null && !isBookmarked) {
       addBookmarks({
         id,
         type,
       });
+      toast.success('Added to your bookmarks');
     } else if (isBookmarked) {
       removeBookmark({
         id,
         type,
       });
+      toast.success('Removed from your bookmarks');
     }
   };
 
   return (
-    <button onClick={clickHandler}>
-      {isBookmarked ? (
-        <AiFillHeart className='text-[#d61a27] h-6 w-6' />
-      ) : (
-        <AiOutlineHeart className='h-6 w-6 ' />
-      )}
-    </button>
+    <>
+      <Toaster />
+      <button onClick={clickHandler}>
+        {isBookmarked ? (
+          <AiFillHeart className='text-[#d61a27] h-6 w-6' />
+        ) : (
+          <AiOutlineHeart className='h-6 w-6 ' />
+        )}
+      </button>
+    </>
   );
 };
 
