50f669aaa6ccd3c9b47582634c6f9c4e71f2feb5
dev-goutham <dev.gouthamram95@gmail.com>
2022-10-18 14:53:31 +0530

    imdb link bug fix

--- src/sections/DetailsSection/index.tsx
+++ src/sections/DetailsSection/index.tsx
@@ -73,16 +73,16 @@ const DetailsSection: React.FC<Props> = ({ id, type }) => {
               />
               <div className='flex mt-2 justify-between'>
                 <div className='flex gap-2'>
-                  {item.external_ids.imdb_id && (
+                  {item.external_ids?.imdb_id && (
                     <Imdb imdbId={item.external_ids.imdb_id} />
                   )}
-                  {item.external_ids.facebook_id && (
+                  {item.external_ids?.facebook_id && (
                     <Facebook handle={item.external_ids.facebook_id} />
                   )}
-                  {item.external_ids.instagram_id && (
+                  {item.external_ids?.instagram_id && (
                     <Instagram handle={item.external_ids.instagram_id} />
                   )}
-                  {item.external_ids.twitter_id && (
+                  {item.external_ids?.twitter_id && (
                     <Twitter handle={item.external_ids.twitter_id} />
                   )}
                 </div>
